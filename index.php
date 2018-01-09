0e94adcceb06d79ad849ffa781f5ccf6d3078241
Ellie <ellie1337@gmail.com>
2018-01-08 22:00:03 -0800

    Allow paths to be configured. Always use forward slashes.

--- index.php
+++ index.php
@@ -4,10 +4,16 @@
         Started January 6th, 2018
     */
 
-    // Paths
-    define('BASE_PATH', realpath('.').'/');
-    define('APPLICATION_PATH', BASE_PATH.'application/');
-    define('SYSTEM_PATH', BASE_PATH.'system/');
+    // Configure
+    $system_path = "system";
+    $application_path = "application";
+
+    // Resolve Paths
+    define('BASE_PATH', str_replace("\\", "/", realpath('.')).'/');
+    define('APPLICATION_PATH', BASE_PATH.$application_path.'/');
+    define('SYSTEM_PATH', BASE_PATH.$system_path.'/');
+	unset($system_path);
+	unset($application_path);
 
     // Initialize System
     require_once(SYSTEM_PATH.'system.php');
