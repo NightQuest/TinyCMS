ea1ebff148e7c1dd3360e6cc392c9062b14e5fc7
Ellie <ellie1337@gmail.com>
2018-01-08 23:56:34 -0800

    Fix tabs

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
-    /*
-        Written by Ellie
-        Started January 6th, 2018
-    */
+	/*
+		Written by Ellie
+		Started January 6th, 2018
+	*/
 
-    // Configure
-    $system_path = "system";
-    $application_path = "application";
+	// Configure
+	$system_path = "system";
+	$application_path = "application";
 
-    // Resolve Paths
-    define('BASE_PATH', str_replace("\\", "/", realpath('.')).'/');
-    define('APPLICATION_PATH', BASE_PATH.$application_path.'/');
-    define('SYSTEM_PATH', BASE_PATH.$system_path.'/');
+	// Resolve Paths
+	define('BASE_PATH', str_replace("\\", "/", realpath('.')).'/');
+	define('APPLICATION_PATH', BASE_PATH.$application_path.'/');
+	define('SYSTEM_PATH', BASE_PATH.$system_path.'/');
 	unset($system_path);
 	unset($application_path);
 
-    // Initialize System
-    require_once(SYSTEM_PATH.'system.php');
-    $System = get_system();
+	// Initialize System
+	require_once(SYSTEM_PATH.'system.php');
+	$System = get_system();
 
 ?>
